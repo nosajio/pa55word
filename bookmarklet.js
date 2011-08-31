132779aa5516ec3687d9cda34e0d63d379680991
Jason Howmans <jasonhowmans@me.com>
2011-08-31 23:55:49 +0100

    Basic functionality working in the background - now onto constructing the UI.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,15 +1,17 @@
 if (typeof jQuery == 'undefined') {
 	var jQ = document.createElement('script');
 	jQ.type = 'text/javascript';
-	jQ.onload=runthis;
+	jQ.onload=bookmarklet;
 	jQ.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
 	document.body.appendChild(jQ);
 } else {
 	bookmarklet();
 }
 
+// Everything happens through this function
 function bookmarklet() {
-	if (typeof jQuery == 'undefined') 
-	var b = $('body');
-	b.append('<div id="pa55word">The Password</div>');
+	$('body').append('<div id="pa55word">The Password</div>');
 }
+
+// Stop the script repeating
+var done = true;
