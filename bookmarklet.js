bd0940f7e6d509977462c9bf956d636a99018a73
Jason Howmans <jasonhowmans@me.com>
2011-09-01 00:32:42 +0100

    Started testing on websites out in the wild

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,4 @@
+// Pull in jQuery
 if (typeof jQuery == 'undefined') {
 	var jQ = document.createElement('script');
 	jQ.type = 'text/javascript';
@@ -10,7 +11,8 @@ if (typeof jQuery == 'undefined') {
 
 // Everything happens through this function
 function bookmarklet() {
-	$('body').append('<div id="pa55word">The Password</div>');
+	$('body').append('<div id="pa55word" style="background: #444; display: none; height: 100px; position: absolute; right: 50%; top: 0px; width: 200px; z-index: 9999;"><h3 style="color: white; text-align: center;">Generated Password</h3><span id="code"></span></div>');
+	$('#pa55word').slideDown();
 }
 
 // Stop the script repeating
